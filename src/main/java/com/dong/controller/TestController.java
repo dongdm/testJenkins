1940b55568673113d787f390fa36077f7b5b9cf2
dongdm <v-dongdm@vanke.com>
2018-04-30 16:01:48 +0800

    init

--- src/main/java/com/dong/controller/TestController.java
+++ src/main/java/com/dong/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.dong.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +14,8 @@ public class TestController {
 	
 	@RequestMapping(value="/getTest")
 	@ResponseBody
-	public String getTest() {
+	public String getTest(HttpServletRequest request, HttpServletResponse response) {
+		response.setCharacterEncoding("utf-8");
 		Test test = new Test();
 		test.setId(1);
 		test.setName("小名小名");
